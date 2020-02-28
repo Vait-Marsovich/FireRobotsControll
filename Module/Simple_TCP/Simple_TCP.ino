09d4425fc4636112b860051db71cb1f2a622e718
Vait-Marsovich <55848496+Vait-Marsovich@users.noreply.github.com>
2020-02-28 09:49:03 +0300

    From HP

--- Module/Simple_TCP/Simple_TCP.ino
+++ Module/Simple_TCP/Simple_TCP.ino
@@ -1,9 +1,12 @@
+#include <SerialESP8266wifi.h>
+#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
+
 WiFiServer server(750);
-char* ssid = "FR";
+char* ssid = "FR"; //char*
 char* password =  "12345FR485";
 IPAddress apIP(192, 168, 4, 1);
 unsigned char RX;
@@ -14,11 +17,11 @@ void setup() {
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   WiFi.softAP(ssid, password, 11, 4, false);
   server.begin();
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.print("Guten tug");
 }
 
-void loop() 
+void loop() {
   //int a = 2;
   //bool x = 0;
   WiFiClient client = server.available();
@@ -29,7 +32,7 @@ void loop()
         client.print(TX); // 
       }
       if (client.available() > 0) { //Если принимаем по TCP
-        char RX = client.read();
+        char RX = client1.read();
         Serial.print(RX);
       }
     }
